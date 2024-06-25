c749c9cd313ae7253e03feb99c7e6b98ed951aff
Venkata Naga Kailash Anantha <avnkailash@gmail.com>
2024-06-26 00:01:55 +0530

    Minor updates to the function names and clearing unwanted files from git tracking

--- app/api.py
+++ app/api.py
@@ -23,14 +23,14 @@ router = APIRouter(prefix="/api", tags=["API"])
 
 
 @router.get("/forms", response_model=FormListResponse)
-def read_forms(db: Session = Depends(get_db)):
+def get_forms_list(db: Session = Depends(get_db)):
     forms = get_forms(db)
     print(forms)
     return {"data": forms}
 
 
 @router.get("/form/{form_id}", response_model=Form)
-def read_form(form_id: str, db: Session = Depends(get_db)):
+def get_form_config(form_id: str, db: Session = Depends(get_db)):
     form = get_form_by_id(db, form_id)
     if form is None:
         raise HTTPException(status_code=404, detail="Form not found")
@@ -43,8 +43,8 @@ def create_form(form: FormCreate, db: Session = Depends(get_db)):
     return create_new_form(db=db, form=form)
 
 
-@router.patch("/forms/{form_id}/activation", response_model=Form)
-def activate_deactivate_form_endpoint(
+@router.patch("/forms/{form_id}/toggle", response_model=Form)
+def toggle_form_status(
     form_id: str, action: FormActivateDeactivate, db: Session = Depends(get_db)
 ):
     form = activate_deactivate_form(db, form_id, action.activate)
@@ -54,7 +54,7 @@ def activate_deactivate_form_endpoint(
 
 
 @router.get("/submissions", response_model=List[Submission])
-def read_submissions(form_id: str = None, db: Session = Depends(get_db)):
+def get_submissions(form_id: str = None, db: Session = Depends(get_db)):
     if not form_id:
         raise HTTPException(status_code=400, detail="form_id is required")
     submissions = get_submissions_by_form_id(db, form_id)
